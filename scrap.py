d31cd040c8883159b75f513cd872de98ccc0833a
Ben <ben.tannenbaum1@gmail.com>
2018-11-14 19:10:35 +0100

    Add files via upload

--- scrap.py
+++ scrap.py
@@ -1,31 +1,94 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+#scrap stock screener from TradingView
 
-option = webdriver.ChromeOptions()
-option.add_argument("incognito")
+import json
+import requests
+from bs4 import BeautifulSoup
 
-browser = webdriver.Chrome(executable_path='C:/Users/Admin/Downloads/chromedriver', chrome_options=option)
+class AppleJobsScraper(object):
+    def __init__(self):
+        self.search_request = {"filter":[{"left":"market_cap_basic","operation":"nempty"},{"left":"type","operation":"in_range","right":["stock","dr","fund"]},{"left":"subtype","operation":"in_range","right":["common","","etf","unit"]},{"left":"exchange","operation":"in_range","right":["NYSE","NASDAQ","AMEX"]}],"symbols":{"query":{"types":[]},"tickers":[]},"columns":["name","close","change","change_abs","Recommend.All","volume","market_cap_basic","price_earnings_ttm","earnings_per_share_basic_ttm","number_of_employees","sector","description","name","type","subtype","pricescale","minmov","fractional","minmove2"],"sort":{"sortBy":"market_cap_basic","sortOrder":"desc"},"options":{"lang":"en"},"range":[0,150]}
 
-browser.get("https://github.com/TheDancerCodes")
+    def scrape(self):
+        jobs = self.scrape_jobs()
+        for job in jobs:
+            print(job)
 
-# Wait 20 seconds for page to load
-timeout = 20
-try:
-    WebDriverWait(browser, timeout).until(EC.presence_of_element_located((By.XPATH, "//*[@id='js-pjax-container']/div/div[1]/a/img")))
-except TimeoutException:
-    print("Timed out waiting for page to load")
-    browser.quit()
+    #def scrape_jobs(self, max_pages=9553):
+    def scrape_jobs(self):
+        jobs = []
+        #totalCount = 0
+        #pageno = 0
+        #self.search_request['totalCount'] = totalCount
+        #self.search_request['pageNumber'] = pageno
 
-    # find_elements_by_xpath returns an array of selenium objects.
-titles_element = browser.find_elements_by_xpath("//a[@class=’text-bold’]")
-# use list comprehension to get the actual repo titles and not the selenium objects.
-titles = [x.text for x in titles_element]
-# print out all the titles.
-print('titles:')
-print(titles, '\n')
+        #while totalCount < max_pages:
 
+        payload = json.dumps(self.search_request)
+            #print(json.loads(payload))
 
-#//*[@id="js-pjax-container"]/div/div[1]/a/img
+        session = requests.Session()
+
+        r = session.post(
+            url='https://scanner.tradingview.com/america/scan',
+            data=payload,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+                'Accept': 'text/plain, */*; q=0.01',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6',
+                'Connection': 'keep-alive',
+                'Content-Length': '685',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    #'Host': 'scanner.tradingview.com',
+                'Origin': 'https://www.tradingview.com',
+                'Referer': 'https://www.tradingview.com/screener/',
+                }
+                        )
+        print(r)
+
+            #print(totalCount + 1)
+
+            #t = BeautifulSoup(r.content,features="lxml")
+        s = BeautifulSoup(r.text,features="lxml")
+
+            #print(totalCount + 2)
+
+            #print(s)
+            #print(t)
+                #print(s.requisition)
+
+            #if not s.requisition:
+                #print(totalCount + 3)
+                #break
+
+            #print(totalCount + 4)
+
+        with open("tview_scan.html", "w") as file:
+            file.write(str(s))
+
+        body = (s.find('p'))
+
+        #content = body.findChildren()
+
+        print(body)
+        #print(content)
+
+        #for r in s.find('body'):
+            #job = {}
+            #print(totalCount + 5)
+            #job['data'] = r.data.text
+                    #job['title'] = r.postingtitle and \
+                    #r.postingtitle.text or r.retailpostingtitle.text
+                    #job['location'] = r.location.text
+            #jobs.append(job)
+
+                # Next page
+            #totalCount += 1
+            #print(totalCount + 6)
+            #self.search_request['totalCount'] = totalCount
+
+        return jobs
+
+if __name__ == '__main__':
+    scraper = AppleJobsScraper()
+    scraper.scrape()
