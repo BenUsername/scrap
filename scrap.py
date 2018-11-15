a7080b5803140dfc37ff9cdd583ba277e1f70bcc
Ben <ben.tannenbaum1@gmail.com>
2018-11-15 18:35:27 +0100

    Add files via upload

--- scrap.py
+++ scrap.py
@@ -6,7 +6,7 @@ os.system('cls') # clear terminal
 import json
 import requests
 from bs4 import BeautifulSoup
-print("1")
+
 class AppleJobsScraper(object):
     def __init__(self):
         self.search_request = {"filter":[{"left":"market_cap_basic","operation":"nempty"},{"left":"type","operation":"in_range","right":["stock","dr","fund"]},{"left":"subtype","operation":"in_range","right":["common","","etf","unit"]},{"left":"exchange","operation":"in_range","right":["NYSE","NASDAQ","AMEX"]}],"symbols":{"query":{"types":[]},"tickers":[]},"columns":["name","close","change","change_abs","Recommend.All","volume","market_cap_basic","price_earnings_ttm","earnings_per_share_basic_ttm","number_of_employees","sector","description","name","type","subtype","pricescale","minmov","fractional","minmove2"],"sort":{"sortBy":"market_cap_basic","sortOrder":"desc"},"options":{"lang":"en"},"range":[0,150]}
@@ -20,7 +20,7 @@ class AppleJobsScraper(object):
 
         payload = json.dumps(self.search_request)
         session = requests.Session()
-        print("2")
+
         r = session.post(
             url='https://scanner.tradingview.com/america/scan',
             data=payload,
